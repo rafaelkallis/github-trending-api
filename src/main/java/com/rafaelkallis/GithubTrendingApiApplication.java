1d263de30f74f5b1160b84e1c22e139337484df5
rafaelkallis <rk@rafaelkallis.com>
2016-09-22 20:34:54 +0200

    inital project

---
+++ src/main/java/com/rafaelkallis/GithubTrendingApiApplication.java
@@ -0,0 +1,12 @@
+package com.rafaelkallis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTrendingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTrendingApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rafaelkallis/GithubTrendingApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rafaelkallis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubTrendingApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
